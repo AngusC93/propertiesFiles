8a72152c87c7764ab1921e801c369fdfb219e242
angus.clarke <angus@DESKTOP-G2H9E3T.localdomain>
2021-03-09 20:24:43 +1030

    this actually works

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,5 @@
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
 import java.util.Properties;
 
 public class Main {
@@ -10,32 +9,52 @@ public class Main {
         try {
             Properties props = new Properties();
             String propertiesFileName = "application.properties";
-            InputStream inputStream = Main.class.getClassLoader().getResourceAsStream(propertiesFileName);
 
-            String simProps = "another.properties";
-            InputStream inStream = Main.class.getClassLoader().getResourceAsStream(simProps);
+//            InputStream inputStream = Main.class.getClassLoader().getResourceAsStream(propertiesFileName);
+            InputStreamReader in = new InputStreamReader(new FileInputStream("target/resources/application.properties"),
+                    StandardCharsets.UTF_8);
+            props.load(in);
+//            String simProps = "another.properties";
+//            InputStream inStream = Main.class.getClassLoader().getResourceAsStream(simProps);
 
-            if (inStream != null) {
-            props.load(inStream);
-            } else {
-                throw new FileNotFoundException("property file '" + simProps + "' not found");
-            }
+//            if (inStream != null) {
+//            props.load(inStream);
+//            } else {
+//                throw new FileNotFoundException("property file '" + simProps + "' not found");
+//            }
 
-            System.out.println(props.getProperty("sim.size"));
-            System.out.println(props.getProperty("sim.host"));
-
-            if (inputStream != null) {
-                props.load(inputStream);
-            } else {
-                throw new FileNotFoundException("property file '" + propertiesFileName + "' not found");
-            }
-
-            System.out.println(props.getProperty("my.age"));
             System.out.println(props.getProperty("my.name"));
+            System.out.println(props.getProperty("my.age"));
+
+//            if (inputStream != null) {
+//                props.load(inputStream);
+//            } else {
+//                throw new FileNotFoundException("property file '" + propertiesFileName + "' not found");
+//            }
+
+//            System.out.println(props.getProperty("my.age"));
+//            System.out.println(props.getProperty("my.name"));
 
         } catch (Exception e) {
             System.out.println("Exception: " + e);
         }
+
+//        try {
+//            Properties props = new Properties();
+//            String yamlFile = "test.yaml";
+//            InputStream inputStream = Main.class.getClassLoader().getResourceAsStream(yamlFile);
+//
+//            if (inputStream != null) {
+//                props.load(inputStream);
+//            } else {
+//                throw new FileNotFoundException("property file '" + yamlFile + "' not found");
+//            }
+//
+//            System.out.println(props.getProperty("name"));
+//            System.out.println(props.getProperty("wage"));
+//        } catch (Exception e) {
+//            System.out.println("Exception: " + e);
+//        }
     }
 
 }
